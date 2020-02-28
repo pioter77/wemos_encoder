c08ff9a9e347759ae157154e4a7a0b8c5f6fe576
Piotr Dziedzinski <piotr.dziedzinski@gmail.com>
2020-02-28 01:26:37 +0100

    wersja z przerwaniem tylko na przycisk

---
+++ src/main.cpp
@@ -0,0 +1,62 @@
+#include <Arduino.h>
+#define pinA 12 //d6
+#define pinB 13 //d7
+#define btn1 14 //d5
+int val;
+int encoder0Pos=0;
+int pinA_last;
+int n=LOW;
+
+volatile bool flag1=0;
+
+void ICACHE_RAM_ATTR isr1()
+{
+noInterrupts();
+flag1=1;
+}
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Basic Encoder Test:");
+  pinMode(pinA,INPUT);
+  pinMode(pinB,INPUT);
+  pinMode(btn1,INPUT_PULLUP);
+  pinA_last=digitalRead(pinA_last);
+  attachInterrupt(digitalPinToInterrupt(btn1),isr1,FALLING);
+   
+}
+
+void loop()
+{
+  if(flag1)
+  {
+    encoder0Pos=0;
+     pinA_last=digitalRead(pinA);
+     Serial.println(encoder0Pos);
+
+     flag1=0;
+    delay(350);
+    interrupts();
+  }
+
+  n=digitalRead(pinA);
+  if((pinA_last==LOW)&& n==HIGH)
+  {
+    //Serial.println("petla1");
+      if(digitalRead(pinB)==LOW)
+      {
+           // Serial.println("petla2");
+
+        encoder0Pos--;
+      }else{
+           // Serial.println("petla3");
+
+        encoder0Pos++;
+      }
+      delay(50);
+      Serial.println(encoder0Pos);
+  }
+  pinA_last=n;
+
+}
